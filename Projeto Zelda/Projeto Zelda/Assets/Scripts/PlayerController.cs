c6d16ba3047c770956a880616599f4a9ed100619
Ricardo Pinas <ricardorieu@gmail.com>
2024-01-06 20:48:32 -0300

    Projeto zelda

---
+++ Projeto Zelda/Projeto Zelda/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private CharacterController controller;
+    private Animator anim;
+
+
+    [Header("Config Player")]
+    public float movementSpeed = 3f;
+    private Vector3 direction;
+    private bool isWalk;
+
+    //Input
+    private float horizontal;
+    private float vertical;
+
+
+    [Header("Attack Config")]
+    public ParticleSystem fxAttack;
+    public Transform hitBox;
+    [Range(0.2f, 1f)]
+    public float hitRange = 0.5f;
+    public LayerMask hitMask;
+    private bool isAttack;
+
+    public Collider[] hitInfo;
+    public int amountDmg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Inputs();
+        
+        MoveCharacter();
+
+        UpdateAnimator();
+        
+    }
+
+    #region MEUS MÉTODOS
+
+    //MÉTODO RESPONSÁVEL PELAS ENTRADAS DE COMANDO DO USUÁRIO
+    void Inputs()
+    {
+         horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Fire1") && isAttack == false)
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        isAttack = true;
+        anim.SetTrigger("Attack");
+        fxAttack.Emit(1);
+
+        hitInfo = Physics.OverlapSphere(hitBox.position, hitRange, hitMask);
+
+
+        foreach(Collider c in hitInfo)
+        {
+            c.gameObject.SendMessage("GetHit", amountDmg, SendMessageOptions.DontRequireReceiver);
+        }
+
+
+
+
+    }
+
+    //MÉTODO RESPONSÁVEL POR MOVER O PERSONAGEM
+    void MoveCharacter()
+    {
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude > 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+            isWalk = true;
+        }
+        else       //if(direction.magnitude <= 0.1f)
+        {
+            isWalk = false;
+        }
+
+        controller.Move(direction * movementSpeed * Time.deltaTime);
+    }
+
+    //MÉTODO RESPONSAVEL EM ATUALIZAR O ANIMATOR
+    void UpdateAnimator()
+    {
+        anim.SetBool("isWalk", isWalk);
+    }
+
+    void AttackIsDone()
+    {
+        isAttack = false;
+    }
+
+
+    #endregion
+
+
+    private void OnDrawGizmosSelected()
+    {
+        if(hitBox != null)
+        {
+            Gizmos.DrawWireSphere(hitBox.position, hitRange);
+        }
+    }
+}
---
+++ Projeto Zelda/Projeto Zelda/Assets/Scripts/SlimeIA.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SlimeIA : MonoBehaviour
+{
+    private GameManager _GameManager;
+
+    private Animator anim;
+    public ParticleSystem hitEffect;
+    public int HP;
+
+    private bool isDie;
+
+    public enemyState state;
+
+    public const float idleWaitTime = 3f;
+    public const float patrolWaitTime = 5f;
+
+    //IA do slime
+    private NavMeshAgent agent;
+    private int idWayPoint;
+    private Vector3 destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _GameManager = FindObjectOfType(typeof(GameManager)) as GameManager;
+
+        anim = GetComponent<Animator>();
+
+        agent = GetComponent<NavMeshAgent>();
+
+        ChangeState(state);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    IEnumerator Died()
+    {
+        isDie = true;
+        yield return new WaitForSeconds(2.5f);
+        Destroy(this.gameObject); 
+    }
+
+    #region MEUS METODOS
+
+    void GetHit(int amount)
+    {
+        if(isDie == true) { return; }
+
+        HP -= amount;
+
+        if(HP > 0)
+        {
+            anim.SetTrigger("GetHit");
+        }
+        else // <=
+        {
+            anim.SetTrigger("Die");
+            StartCoroutine("Died");
+        }
+
+
+        anim.SetTrigger("GetHit");
+        hitEffect.Emit(25);
+    }
+
+
+    void StateManager()
+    {
+        switch (state)
+        {
+            case enemyState.IDLE:
+                //COMPORTAMENTO QUANDO ESTIVER EM IDLE , PARADO
+                break;
+
+            case enemyState.ALERT:
+                //COMPORTAMENTO QUANDO ESTIVER EM ALERTA
+                break;
+
+            case enemyState.EXPLORE:
+                //COMPORTAMENTO QUANDO ESTIVER EXPLORANDO
+                break;
+
+            case enemyState.FOLLOW:
+                //COMPORTAMENTO QUANDO ESTIVER SEGUINDO
+                break;
+
+            case enemyState.FURY:
+                //COMPORTAMENTO QUANDO ESTIVER EM FURIA
+                break;
+
+            case enemyState.PATROL:
+                //COMPORTAMENTO QUANDO ESTIVER PATRULHANDO
+                break;
+        }
+    }
+
+    void ChangeState(enemyState newState)
+    {
+        StopAllCoroutines(); //ENCERRA TODAS AS CORROUTINAS
+        state = newState;
+        print(newState);
+
+        switch (state)
+        {
+            case enemyState.IDLE:
+
+                destination = transform.position;
+                agent.destination = destination;
+
+                StartCoroutine("IDLE");
+
+                break;
+
+            case enemyState.ALERT:
+                
+                break;
+
+            case enemyState.PATROL:
+
+                idWayPoint = Random.Range(0, _GameManager.slimeWayPoints.Length);
+                destination = _GameManager.slimeWayPoints[idWayPoint].position;
+                agent.destination = destination;
+
+                StartCoroutine("PATROL");
+
+                break;
+        }
+    }
+
+    IEnumerator IDLE()
+    {
+        yield return new WaitForSeconds(idleWaitTime);
+
+        StayStill(50); //50% de chance de ficar parado ou entrar em patrulha
+    }
+
+    IEnumerator PATROL()
+    {
+        yield return new WaitForSeconds(patrolWaitTime);
+
+        StayStill(30); // 30% de chance de ficar parado e 70% de ficar em patrulha
+
+    }
+
+    void StayStill(int yes)
+    {
+        if(Rand() <= yes)
+        {
+            ChangeState(enemyState.IDLE);
+        }
+        else // CASO NO
+        {
+            ChangeState(enemyState.PATROL);
+        }
+    }
+
+    int Rand()
+    {
+        int rand = Random.Range(0, 100); // 0, 1, 2, 3... 99.
+        return rand;
+    }
+
+
+
+    #endregion
+}
